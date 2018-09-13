79d8532645a990051b7414176af17f268e7bd55f
alloky <alloky2011@gmail.com>
2018-09-13 09:10:28 +0300

    hpp added

---
+++ src/utility.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+namespace utility {
+	
+	template <typename T>
+	void cout_array(const std::string& msg, size_t size, T* arr){
+		std::cout << "# " << msg << "n# ";
+		for(int i = 0; i < size; ++i){
+			std::cout << arr[i] << " "; 
+		}
+		std::cout << "\n############";
+	}
+}
