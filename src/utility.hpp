727af98dacf2f998a3c2c2ea11dd256253b79c2f
alloky <alloky2011@gmail.com>
2018-09-15 14:31:57 +0300

    asserts added

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include <assert.h>
 
 namespace utility {
 	
 	template <typename T>
 	void cout_array(const std::string& msg, size_t size, T* arr){
+		assert(arr != NULL);
 		std::cout << "# " << msg << " #\n# ";
 		for(size_t i = 0; i < size; ++i){
 			std::cout << arr[i] << " "; 
